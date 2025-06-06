e0a2b3e5982c3680c84548caecdc39bd6fa4bbc0
Lazzzare <164561286+LazareChkhartishvili@users.noreply.github.com>
2025-06-06 23:57:43 +0400

    make you win container and almost done

--- src/app/components/WordGame.tsx
+++ src/app/components/WordGame.tsx
@@ -24,17 +24,19 @@ const WordGame = ({ category }: { category: string }) => {
   const [disabledLetters, setDisabledLetters] = useState<string[]>([]);
   const [gamePauseMenu, setGamePauseMenu] = useState(false);
   const [health, setHealth] = useState<number>(100);
+  const [isWinner, setIsWinner] = useState<boolean>(false);
 
   const router = useRouter();
-
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
   const getHealthWidthClass = (health: number): string => {
     if (health > 90) return "w-[100%]";
-    if (health > 60) return "w-[70%]";
-    if (health > 30) return "w-[50%]";
-    if (health > 10) return "w-[30%]";
-    if (health > 0) return "w-[10%]";
+    if (health > 75) return "w-[90%]";
+    if (health > 60) return "w-[75%]";
+    if (health > 45) return "w-[60%]";
+    if (health > 30) return "w-[45%]";
+    if (health > 15) return "w-[30%]";
+    if (health > 0) return "w-[15%]";
     return "w-[0%]";
   };
 
@@ -53,12 +55,23 @@ const WordGame = ({ category }: { category: string }) => {
     loadWord();
   }, [loadWord]);
 
+  useEffect(() => {
+    if (word && word !== "") {
+      const lettersInWord = word.replace(/\s/g, "").split("");
+      const uniqueLetters = [...new Set(lettersInWord)];
+      const hasWon = uniqueLetters.every((char) =>
+        correctLetters.includes(char)
+      );
+      setIsWinner(hasWon);
+    }
+  }, [correctLetters, word]);
+
   const handleLetterClick = (letter: string) => {
     if (word.includes(letter)) {
       setCorrectLetters((prev) => [...prev, letter]);
     } else {
       setDisabledLetters((prev) => [...prev, letter]);
-      setHealth((prev) => Math.max(prev - 30, 0));
+      setHealth((prev) => Math.max(prev - 15, 0));
     }
   };
 
@@ -66,6 +79,7 @@ const WordGame = ({ category }: { category: string }) => {
     setCorrectLetters([]);
     setDisabledLetters([]);
     setHealth(100);
+    setIsWinner(false);
     setGamePauseMenu(false);
     loadWord();
   };
@@ -92,6 +106,45 @@ const WordGame = ({ category }: { category: string }) => {
                 width={320}
                 height={161}
                 alt="you_lose_text"
+              />
+              <div className="flex flex-col items-center gap-8">
+                <div
+                  onClick={resetGame}
+                  className="bg-[url('/images/word-game-images/ButtonHover.png')] hover:scale-105 duration-500 cursor-pointer w-[226px] h-[62px] bg-contain bg-no-repeat flex justify-center items-center"
+                >
+                  <h2 className="text-white uppercase text-[32px]">
+                    Play Again
+                  </h2>
+                </div>
+                <div
+                  onClick={() => router.push("/pickCategory")}
+                  className="bg-[url('/images/word-game-images/ButtonHover.png')] hover:scale-105 duration-500 cursor-pointer w-[275px] h-[62px] bg-contain bg-no-repeat flex justify-center items-center"
+                >
+                  <h2 className="text-white uppercase text-[32px]">
+                    New Category
+                  </h2>
+                </div>
+                <div
+                  onClick={() => router.push("/")}
+                  className="bg-[url('/images/word-game-images/GradientButtonHover.png')] hover:scale-105 duration-500 cursor-pointer w-[235px] h-[62px] bg-contain bg-no-repeat flex justify-center items-center"
+                >
+                  <h2 className="text-white uppercase text-[32px]">Quit</h2>
+                </div>
+              </div>
+            </Container>
+          </div>
+        </>
+      ) : isWinner ? (
+        <>
+          <div className="absolute inset-0 bg-black opacity-70 z-40" />
+          <div className="absolute inset-0 z-50 flex justify-center items-center">
+            <Container>
+              <Image
+                className="absolute -top-6"
+                src="/images/word-game-images/youWin.png"
+                width={320}
+                height={161}
+                alt="you_win_text"
               />
               <div className="flex flex-col items-center gap-8">
                 <div
